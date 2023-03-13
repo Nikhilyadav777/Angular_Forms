2efe1de3afc7542390585b4dc39488725d66caa3
Nikhil <107390052+Nikhilyadav777@users.noreply.github.com>
2023-03-13 13:12:35 +0530

    Add files via upload
    Adding the test cases for the form

--- form/form.component.spec.ts
+++ form/form.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
 
@@ -21,5 +21,80 @@ describe('FormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+});
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { FormComponent } from './form.component';
+
+describe('FormComponent', () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormComponent ],
+      imports: [ ReactiveFormsModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should render first name input', () => {
+    const firstNameInput = fixture.debugElement.query(By.css('#firstName'));
+    expect(firstNameInput).toBeTruthy();
+  });
+
+  it('should require first name input', () => {
+    const firstNameInput = component.myForm.controls['firstName'];
+    firstNameInput.setValue('');
+    expect(firstNameInput.valid).toBeFalsy();
+  });
+
+  it('should validate first name input', () => {
+    const firstNameInput = component.myForm.controls['firstName'];
+    firstNameInput.setValue('1234');
+    expect(firstNameInput.valid).toBeFalsy();
+  });
+
+  it('should render email input', () => {
+    const emailInput = fixture.debugElement.query(By.css('#email'));
+    expect(emailInput).toBeTruthy();
+  });
+
+  it('should require email input', () => {
+    const emailInput = component.myForm.controls['email'];
+    emailInput.setValue('');
+    expect(emailInput.valid).toBeFalsy();
+  });
+
+  it('should validate email input', () => {
+    const emailInput = component.myForm.controls['email'];
+    emailInput.setValue('invalid-email');
+    expect(emailInput.valid).toBeFalsy();
+  });
+
+  it('should render numeric field input', () => {
+    const numericFieldInput = fixture.debugElement.query(By.css('#numericField'));
+    expect(numericFieldInput).toBeTruthy();
+  });
+
+  it('should require numeric field input', () => {
+    const numericFieldInput = component.myForm.controls['numericField'];
+    numericFieldInput.setValue('');
+    expect(numericFieldInput.valid).toBeFalsy();
+  });
+
+  it('should validate numeric field input', () => {
+    const numericFieldInput = component.myForm.controls['numericField'];
+    numericFieldInput.setValue('abc');
+    expect(numericFieldInput.valid).toBeFalsy();
   });
 });
